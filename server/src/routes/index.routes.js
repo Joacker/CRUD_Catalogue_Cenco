98a9859ccf5ba3fe800e50999905d818a26b555a
Joacker <jfernandezvizcarra@gmail.com>
2023-01-30 12:37:44 -0300

    testing

--- server/src/routes/index.routes.js
+++ server/src/routes/index.routes.js
@@ -1,8 +1,13 @@
+// import the package json file
+const pkg = require('../../package.json');
 const {Router} = require('express');
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.send('Hello World asdasdsa');
+    res.json({message: 'Welcome to my API'});
+    // print the package json file
+    console.log(pkg);
+    //res.send('Hello World asdasdsa');
 });
 
 module.exports = router;
